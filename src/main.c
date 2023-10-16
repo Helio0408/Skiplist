31348c449cb31e7b56cf34f5ead30704c3736799
Gabriel Martins <gabrielmartinsthe12381@gmail.com>
2023-10-16 18:35:03 -0300

    adicionado escopo do programa principal

--- src/main.c
+++ src/main.c
@@ -6,8 +6,40 @@ int main(){
 	srand(time(NULL));
 
 	lista *l;
-
+	char cmd[256];
+	char verbete[tam_verb + 1];
+	char definicao[tam_def + 1];
+	char character;
 	l = criar_lista();
+
+	while(scanf("%s", cmd) == 1){
+		if(!strcmp(cmd, "busca")){
+			scanf("%s", verbete);
+			busca_definicao(l, verbete);
+		}
+		else if(!strcmp(cmd, "insercao")){
+			scanf("%s ", verbete);
+			fgets(definicao, tam_def + 1, stdin);
+			definicao[strlen(definicao) - 1] = '\0'; // remover o '\n' que o fgets captura
+
+			insercao(l, verbete, definicao);
+		}
+		else if(!strcmp(cmd, "alteracao")){
+			scanf("%s ", verbete);
+			fgets(definicao, tam_def + 1, stdin);
+			definicao[strlen(definicao) - 1] = '\0'; // remover o '\n' que o fgets captura
+
+			alteracao(l, verbete, definicao);
+		}
+		else if(!strcmp(cmd, "remocao")){
+			scanf("%s", verbete);
+			remocao(l, verbete);
+		}
+		else if(!strcmp(cmd, "impressao")){
+			scanf(" %c", &character);
+			impressao(l, character);
+		}
+	}
 
 	liberar_lista(l);
 	return 0;
